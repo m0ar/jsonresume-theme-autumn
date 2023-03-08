5ff75323534e4efbc669fc817c6aa87ec2e98bb2
m0ar <edvard@hubinette.me>
2023-03-08 14:11:06 +0100

    Use color alias instead of hardcoded orange

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 const { VIEWS_DIR } = require('./build-config');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: [`${VIEWS_DIR}/**/*.hbs`],
   theme: {
     fontFamily: {
       sans: ['Ubuntu', 'ui-sans-serif', 'sans-serif']
+    },
+    colors: {
+      ...colors,
+      primary: colors.orange,
     },
     extend: {}
   },
